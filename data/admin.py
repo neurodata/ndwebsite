71bc8e09fe23171f5e8f2bbfe9a73dc7aa3873c1
Alex Baden <alex.baden@gmail.com>
2016-10-11 23:13:34 -0400

    data admin hook, models v0.0001

--- data/admin.py
+++ data/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import DataProject, ExploreTile
+
 # Register your models here.
+class DataProjectAdmin(admin.ModelAdmin):
+    fields = ('title', 'desc', 'info')
+
+admin.site.register(DataProject, DataProjectAdmin)
--- data/models.py
+++ data/models.py
@@ -3,3 +3,27 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class DataProject(models.Model):
+    title = models.CharField(max_length=255, blank=True)
+    desc = models.TextField(blank=True)
+    info = models.TextField(blank=True)
+
+class ExploreTile(models.Model):
+    title = models.CharField(max_length=255, blank=True)
+    author = models.CharField(max_length=255, blank=True)
+    modality = models.CharField(max_length=255, blank=True)
+    species = models.CharField(max_length=255, blank=True)
+    cuboids = models.CharField(max_length=255, blank=True)
+    cuboid_size = models.CharField(max_length=255, blank=True)
+    voxels = models.CharField(max_length=255, blank=True)
+
+    image = models.ImageField(upload_to='/upload/tile/explore/')
+
+    SUBCATEGORY_CHOICES = (
+        ('Images', 'Images'),
+        ('Time', 'Time'),
+        ('Matrices', 'Matrices'),
+        ('Shapes', 'Shapes'),
+        ('Graphs', 'Graphs')
+    )
+    subcategory = models.CharField(max_length=255, choices=SUBCATEGORY_CHOICES)
