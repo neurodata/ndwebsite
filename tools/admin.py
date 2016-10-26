4151d8c040865cc254d68d067c7af5eb2d84a541
Eric Perlman <perlman@gmail.com>
2016-10-26 17:11:35 -0400

    Add fields for tool info (based on sample site).

--- tools/admin.py
+++ tools/admin.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 from .models import Tool, ToolType
 
 class ToolAdmin(admin.ModelAdmin):
-    fields = ('token', 'longname', 'short_description', 'long_description', 'types')
+    fields = ('token', 'longname', 'short_description', 'long_description',
+        'list_image_src', 'header_image_src', 'api_text', 'code_text',
+        'doc_text', 'ref_text', 'press_text', 'contributing_text',
+        'types')
     list_display = ('token', 'longname')
     filter_horizontal = ('types',)
 
--- tools/models.py
+++ tools/models.py
@@ -18,6 +18,15 @@ class Tool(models.Model):
     short_description = models.TextField(blank=True)
     long_description = models.TextField(blank=True)
 
+    list_image_src = models.CharField(max_length=128, default="/assets/img/tools/placeholder.png")
+    header_image_src = models.CharField(max_length=128, default="/assets/img/tools/default_header.jpg")
+    api_text = models.TextField(blank=True)
+    code_text = models.TextField(blank=True)
+    doc_text = models.TextField(blank=True)
+    ref_text = models.TextField(blank=True)
+    press_text = models.TextField(blank=True)
+    contributing_text = models.TextField(blank=True)
+
     created = models.DateTimeField(auto_now_add=True) # update automatically on creation
     updated = models.DateTimeField(auto_now=True) # update automatically on save
 
