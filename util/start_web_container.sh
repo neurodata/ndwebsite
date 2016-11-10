b74f90e35ec378c2592f11300a8ed904ff2fa04f
Alex Baden <abaden1@jhu.edu>
2016-11-10 10:25:48 -0500

    Start ssl web container on both http & https ports.

--- util/start_web_container.sh
+++ util/start_web_container.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+WEBSITE_IP=128.220.176.10
+
 docker run -d \
 	--volumes-from ndwebmysqldir \
 	-v /var/www/neurodata.io/ndwebsite:/var/www/ndwebsite/ \
-	-p 128.220.176.10:80:80 \
+	-v /etc/ssl/private/neurodata.io:/etc/ssl/private/neurodata.io/ \
+	-p $WEBSITE_IP:80:80 \
+	-p $WEBSITE_IP:443:443 \
 	--name ndwebsite \
 	--restart=always \
-	ndwebsite:prod 
+	ndwebsite:ssl
